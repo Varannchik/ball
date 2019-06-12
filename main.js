2a3ab212fc8f0926a817df6d87885123cc600936
Varannchik <varannchik@gmail.com>
2019-06-12 22:50:13 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,54 @@
 let ball = document.querySelector('.ball');
+let sq = document.querySelector('.sq');
+let cont= document.querySelector('.container');
+let container = cont.getBoundingClientRect();
+let sqCord = sq.getBoundingClientRect();  
+let sqSizeX=0;
+let sqSizeY=0;
+let widthSq=0;
+let higSQ=0;
 
 ball.addEventListener('mousedown', mouseDown);
 ball.addEventListener('mouseup', mouseUp);
 
-function mouseDown(e){
-    
+function mouseDown(e){ 
+    console.log('start');   
     ball.style.position = 'absolute';
-    move(e);  
-    ball.style.zIndex = 1000;
+    sqSizeY = sqCord.top ;
+    sqSizeX = sqCord.left;
+    widthSq = sqCord.bottom; 
+    higSQ = sqCord.right;   
     
-    function move(e) {
-        ball.style.left = e.pageX - ball.offsetWidth / 2 + 'px';
-        ball.style.top = e.pageY - ball.offsetHeight / 2 + 'px';      
-    }
-    
+    move(e);
     ball.addEventListener('mousemove',function(e){
         move(e); 
     });    
-} 
+}
+
+function move(e) {
+    let a=e.clientX - ball.offsetWidth / 2 ;
+    let b=e.clientY - ball.offsetHeight / 2;
+
+    if(a<=sqSizeX || a>=higSQ-ball.offsetHeight){
+        ball.style.left==sqSizeX + 'px';
+    } else{
+        a=e.clientX - ball.offsetWidth / 2 ;        
+        ball.style.left = a + 'px';
+    }
+    
+    if(b<=sqSizeY || b>=widthSq-ball.offsetWidth){
+        ball.style.top==sqSizeY + 'px';
+    } else{
+        b= e.clientY - ball.offsetHeight / 2;       
+        ball.style.top = b + 'px';        
+    }   
+    
+}
 
 function mouseUp(){
     console.log('end');   
-    ball.removeEventListener('mousemove');
-    ball.removeEventListener('mousedown', mouseDown);   
-   
+    ball.removeEventListener('mousemove', move);
+    ball.removeEventListener('mousedown', mouseDown); 
 }
 
 
